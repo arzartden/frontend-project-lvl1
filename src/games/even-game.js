befe96333130a512ec98bc5601cf578a203dbacf
Arthur Arzhannikov <arzartden@yandex.ru>
2021-11-06 14:53:45 +0700

    edit even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -11,36 +11,27 @@ const checkEven = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let choiceNumUser;
-  let answer;
-  let result = 0;
-  const getAnswer = () => {
-    choiceNumUser = getRandomIntInclusive(1, 100);
+  for (let i = 1; i <= 3; i += 1) {
+    const choiceNumUser = getRandomIntInclusive(1, 100);
     console.log(`Question: ${choiceNumUser}`);
-    answer = readlineSync.question('Your answer: ');
-  };
-  getAnswer();
-  while (result < 2) {
-    if (answer === 'yes' && choiceNumUser % 2 === 0) {
-      console.log('Correct!');
-      result += 1;
-      getAnswer();
-    } else if (answer === 'no' && choiceNumUser % 2 !== 0) {
-      console.log('Correct!');
-      result += 1;
-      getAnswer();
-    } else if (answer === 'yes' && choiceNumUser % 2 !== 0) {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. \n Let's try again, ${name}!`);
-      return;
-    } else if (answer === 'no' && choiceNumUser % 2 === 0) {
-      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. \n Let's try again, ${name}!`);
-      return;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer should have been 'yes' or 'no'.`);
-      return;
+    const answer = readlineSync.question('Your answer: ');
+    if (choiceNumUser % 2 === 0) {
+      if (answer === 'yes') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'. \n Let's try again, ${name}!`);
+        return;
+      }
+    }
+    if (choiceNumUser % 2 !== 0) {
+      if (answer === 'no') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'. \n Let's try again, ${name}!`);
+        return;
+      }
     }
   }
-  console.log('Correct!');
   console.log(`Congratulations, ${name}!`);
 };
 
